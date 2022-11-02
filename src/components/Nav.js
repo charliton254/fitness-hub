b834fdc19df5015f314ff1e9e8bde8e9e64e1bab
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-11-02 22:42:52 +0300

    navigation styled links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,14 @@ function Nav() {
   return (
     <div className='nav'>
         <div className='logo'>
-            Logo
+          <img src='./images/logo.png' style={{height:"80%", paddingRight:"3%"}} alt='logo'/>
+          <h2 style={{ color:"white"}}>Fitness Hub</h2>
         </div>
         <div className='nav-div'>
-        <Link to='/exercises' className='navlinks'>Exercises</Link>
-        <Link to='/plan' className='navlinks'>Plan</Link>
-        <Link to='/meals' className='navlinks'>Meals</Link>
-        <Link to='/timer' className='navlinks'>Timer</Link>
+          <Link to='/exercises' className='navlinks'>Exercises</Link>
+          <Link to='/plan' className='navlinks'>Plan</Link>
+          <Link to='/meals' className='navlinks'>Meals</Link>
+          <Link to='/timer' className='navlinks'>Timer</Link>
         </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,8 @@ function Navbar() {
   return (
     <div className='navbar'>
         <div className='logo'>
-            Logo
+            <img src='./images/logo.png' style={{height:"80%", paddingRight:"3%"}} alt='logo'/>
+            <h2 style={{ color:"white"}}>Fitness Hub</h2>
         </div>
         <div className='navlinks-div'>
             <Link to='/' className='navbarlinks'>Home</Link>
