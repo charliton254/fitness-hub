852bd029497433d0f60c1b68f701f7572cabbd14
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-11-21 19:41:20 +0300

    exercise module

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '../components/Nav';
+import Banner from '../components/Banner';
+import SearchExercises from '../components/SearchExercises';
+import ExerciseResults from '../components/ExerciseResults';
+import Footer from '../components/Footer';
 
 function Exercises() {
+  const [exercises, setExercises] = useState([]);
+
   return (
     <div>
         <Nav/>
-        Exercises
+        <div className='exercise-body'>
+        <Banner/>
+        <SearchExercises setExercises={setExercises}/>
+        <ExerciseResults setExercises={setExercises} exercises={exercises}/>
+        </div>
+        <Footer/>
     </div>
   )
 }
