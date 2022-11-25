94406764c327c2738ef37b237dd9ed19b2cc119f
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-11-25 22:24:50 +0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='cont7logo'>     
+            <img src='./images/logo.png' alt='logo'/>
+            <h4 style={{ color:"white"}}>Fitness Hub</h4>
+        </div>
+        <p>Copyright © 2022 Fitness Hub. All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
