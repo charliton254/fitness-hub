6dcee03fa0b57ffe2692808f9b7e0b3d0b48f9b0
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-11-28 23:51:36 +0300

    meals module

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import Footer from '../components/Footer';
+import MealOptions from '../components/MealOptions';
 import Nav from '../components/Nav';
 
 function Meals() {
   return (
     <div>
         <Nav/>
-        Meals
+        <div className='meals-bar'>
+          <h1>A healthy workout requires a healthy meal</h1>
+          <h2>Take your fitness to the next level by customizing your workouts with our great collection of meals.</h2>
+          <h3>Stay healthy, stay fit.</h3>
+        </div>
+        <MealOptions/>
+        <Footer/>
     </div>
   )
 }
