366ce3b35154b73537ac190d635ad5f2b34c3e31
charliton254 <55441672+charliton254@users.noreply.github.com>
2022-11-30 11:46:12 +0300

    app configuration

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Exercises from './pages/Exercises';
 import Plan from './pages/Plan';
 import Meals from './pages/Meals';
 import Timer from './pages/Timer';
+import ExerciseDetail from './pages/ExerciseDetail';
+import { AuthProvider } from './contexts/AuthContext';
+import LoseWeight from './components/LoseWeight';
+import GainWeight from './components/GainWeight';
+import BuildMuscle from './components/BuildMuscle';
 
 function App() {
   return (
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login/>} />
@@ -17,7 +23,12 @@ function App() {
         <Route path='/plan' element={<Plan/>} />
         <Route path='/meals' element={<Meals/>} />
         <Route path='/timer' element={<Timer/>} />
+        <Route path='/exercise/:id' element={<ExerciseDetail/>} />
+        <Route path='LoseWeight' element={<LoseWeight/>} />
+        <Route path='GainWeight' element={<GainWeight/>} />
+        <Route path='BuildMuscle' element={<BuildMuscle/>} />
       </Routes>   
+    </AuthProvider>
   );
 }
 
